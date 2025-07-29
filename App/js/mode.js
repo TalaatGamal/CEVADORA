a8a7b3cf0a6e7f4b8c6d1c3c11dd0f88ee2b7cb4
TalaatGamal <talaatgamal.dev@gmail.com>
2025-07-29 19:51:25 +0300

    ki

--- App/js/mode.js
+++ App/js/mode.js
@@ -65,3 +65,91 @@ let begin_btn = document.getElementById("tr");
     });
 
 
+
+
+
+
+
+
+
+
+
+
+// responsive design
+// responsive design
+// responsive design
+// responsive design
+// responsive design
+// responsive design
+// responsive design
+// responsive design
+
+
+
+
+
+
+
+let night2 = document.getElementById("night2");
+let daytime2 = document.getElementById("daytime2");
+
+// let body = document.getElementById("body");
+// let header = document.getElementById("header");
+let pri2 = document.getElementById("pri2");
+let header_i2 = document.querySelectorAll("header i");
+let begin_btn2 = document.getElementById("tr");
+// let ani_line = document.querySelectorAll(".ani-line");
+
+    night2.style.display="block"
+    daytime2.style.display="none"
+
+    night2.addEventListener("click", function() {
+        daytime2.style.display = "block";
+        night2.style.display = "none";
+
+        body.style.background="linear-gradient(330deg , #00d6c1 -30% , rgb(0, 0, 0) ,rgb(0, 0, 0) , #00d6c1 125%)";
+        body.style.color="rgb(188, 212, 214)"
+
+        header.style.background="rgba(0, 0, 0, 0.515)"
+
+        pri2.style.background="#006e6b"
+
+        header_i2.forEach(function(el) {
+        el.style.color = "#74e6ff";
+        });
+
+        begin_btn2.style.background="#001e2f6e"
+        
+        // ani_line.forEach(function(ol) {
+        // ol.style.backgroundColor = " #00ffaa";
+        // });
+
+        
+        
+        console.log("Night")
+    });
+    daytime2.addEventListener("click", function() {
+        night2.style.display = "block";
+        daytime2.style.display = "none";
+        
+        body.style.background="linear-gradient(330deg , #00d6c1 -30% , #C6D9DA ,#C6D9DA , #00d6c1 125%)";
+        body.style.color="rgb(53, 66, 65)"
+
+        header.style.background="rgba(221, 233, 230, 0.515)"
+
+        pri2.style.background="#dee9e9"
+
+        header_i2.forEach(function(el) {
+        el.style.color = " #418696";
+        });
+
+        begin_btn2.style.background=" #d3d9dca6"
+        
+        // ani_line.forEach(function(ol) {
+        // ol.style.backgroundColor = " #418696";
+        // });
+
+        console.log("DayTime")
+    });
+
+
